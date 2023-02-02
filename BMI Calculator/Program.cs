4b609a350202cb23750a86fe2a9c0b9251394024
Von VanMany <vonvanmany@gmail.com>
2023-02-01 20:03:29 -0500

    Added Title

---
+++ BMI Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿// Initaialize Vaiable
+
+string name, weight, str_feet, str_inches;
+double pounds, feet, inches, total_inches, BMI;
+
+// set title of program
+
+Console.Title = "BMI Calculator";
+Console.WriteLine(Console.Title);
+
+// Welcome User
+Console.WriteLine("\tWelcome to the " + Console.Title + "program.");
+Console.WriteLine("I'm going to ask you a few Questions " + "and estimate your Body Mass Index");
+
+
+
+
+
+
+// exit the program
+Console.Write("Press enter to quit.");
+Console.Read();
+
+
+
+  
